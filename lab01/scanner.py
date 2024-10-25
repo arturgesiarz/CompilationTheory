5a17a03ea0be6cc9a5a419679d4c96fb26ddd696
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-10-25 12:34:02 +0200

    Lab01 finished

--- lab01/scanner.py
+++ lab01/scanner.py
@@ -1,87 +1,99 @@
+import ply.lex as lex
 
-from sly import Lexer
+# Definicja zarezerowanych słów kluczowych
+reserved = {
+    'if': 'IF',
+    'else': 'ELSE',
+    'for': 'FOR',
+    'while': 'WHILE',
+    'break': 'BREAK',
+    'continue': 'CONTINUE',
+    'return': 'RETURN',
+    'eye': 'EYE',
+    'zeros': 'ZEROS',
+    'ones': 'ONES',
+    'print': 'PRINT'
+}
 
-class MatrixLexer(Lexer):
-    tokens = {
-        ID, INTNUM, FLOATNUM,
-        EYE, ZEROS, ONES,
-        PRINT, IF, ELSE, FOR, WHILE,
-        BREAK, CONTINUE, RETURN,
-        ASSIGN, ADD, SUB, MUL, DIV,
-        DOTADD, DOTSUB, DOTMUL, DOTDIV,
-        LPAREN, RPAREN, LBRACE, RBRACE,
-        LBRACK, RBRACK, COLON, SEMICOLON,
-        COMMA, TRANSPOSE,
-        NEQ, EQ, LT, GT, LEQ, GEQ,
-        STRING
-    }
+# Lista nazw tokenow
+tokens = [
+             'ID',
+             'DOTADD', 'DOTSUB', 'DOTMUL', 'DOTDIV',
+             'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN',
+             'LT', 'GT', 'LE', 'GE', 'EQ', 'NE',
+             'INTNUM',
+             'FLOATNUM',
+             'STRING'
+         ] + list(reserved.values())
 
-    ignore = ' \t'
+# Definiowanie znakow specjalnych
+literals = ['+', '-', '*', '/', '=', '(', ')', '[', ']', '{', '}', ':', '\'', ',', ';']
 
-    # Komentarze
-    @_(r'#.*')
-    def ignore_comment(self, t):
-        pass
+# Definiowanie operatorow macierzy
+t_DOTADD = r'\.\+'
+t_DOTSUB = r'\.-'
+t_DOTMUL = r'\.\*'
+t_DOTDIV = r'\./'
 
-    # Zdefiniowane leksemy
-    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    INTNUM = r'\d+'
-    FLOATNUM = r'\d+\.\d*([eE][-+]?\d+)?'  # Ulepszona obsługa liczb zmiennoprzecinkowych
-    STRING = r'"([^"\\]*(\\.[^"\\]*)*)"'  # Rozpoznawanie ciągów znakowych
+# Definiowanie operatorow przypisania
+t_ADDASSIGN = r'\+='
+t_SUBASSIGN = r'-='
+t_MULASSIGN = r'\*='
+t_DIVASSIGN = r'/='
 
-    EYE = r'eye'
-    ZEROS = r'zeros'
-    ONES = r'ones'
-    PRINT = r'print'
-    IF = r'if'
-    ELSE = r'else'
-    FOR = r'for'
-    WHILE = r'while'
-    BREAK = r'break'
-    CONTINUE = r'continue'
-    RETURN = r'return'
+# Definiowanie operatorow porownania
+t_LT = r'<'
+t_GT = r'>'
+t_LE = r'<='
+t_GE = r'>='
+t_EQ = r'=='
+t_NE = r'!='
 
-    ASSIGN = r'='
-    ADD = r'\+'
-    SUB = r'-'
-    MUL = r'\*'
-    DIV = r'/'
+# Ignorowanie bialych znakow
+t_ignore = ' \t'
 
-    DOTADD = r'\.\+'
-    DOTSUB = r'\.-'
-    DOTMUL = r'\.\*'
-    DOTDIV = r'\./'
+# Ignorowanie komentarzy
+t_ignore_COMMENTS = r'\#.*'
 
-    LPAREN = r'\('
-    RPAREN = r'\)'
-    LBRACE = r'\{'
-    RBRACE = r'\}'
-    LBRACK = r'\['
-    RBRACK = r'\]'
-    COLON = r':'
-    SEMICOLON = r';'
-    COMMA = r','
-    TRANSPOSE = r'\''
 
-    NEQ = r'!='
-    EQ = r'=='
-    LT = r'<'
-    GT = r'>'
-    LEQ = r'<='
-    GEQ = r'>='
+# Reguła dla identyfikatorów, które mogą być zarezerwowanymi słowami lub zmiennymi.
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'ID')  # Check for reserved words
+    return t
 
-    # Funkcja do zwracania tokenów
-    def error(self, t):
-        print(f'Niepoprawny token: {t.value[0]} w linii {t.lineno}')
-        self.index += 1
 
-if __name__ == '__main__':
-    lexer = MatrixLexer()
-    
-    with open('lab01/example.txt', 'r') as file:
-        code = file.read()
-        
-    with open('lab01/output.txt', 'w') as output_file:
-        # Tokenizacja odczytanego kodu i zapis do pliku
-        for token in lexer.tokenize(code):
-            output_file.write(f'{token}\n')  # Zapisz każdy token w nowej linii
+# Regułę dla liczb zmiennoprzecinkowych
+def t_FLOATNUM(t):
+    r'((\d+\.\d*)|(\.\d+))([eE][+-]?\d+)?|(\d+)([eE][+-]?\d+)'
+    t.value = float(t.value)
+    return t
+
+# Regułę dla liczb calkowitych
+def t_INTNUM(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+# Reguła dla ciągów znaków
+def t_STRING(t):
+    r'".*?"'
+    t.value = str(t.value)
+    return t
+
+
+# Reguła dla nowej linii
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+# Reguła dla błędów
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+# Budowanie lexera
+lexer = lex.lex()
